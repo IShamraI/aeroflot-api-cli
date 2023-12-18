92a6135be93da0ad317dda275f2295c1d6910f3f
Ivan Shamrai <isshamray@gmail.com>
2023-12-18 11:47:09 +0300

    Clean

--- cmd/exchange.go
+++ cmd/exchange.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+Copyright © 2023 Ivan Shamrai isshamray@gmail.com
 */
 package cmd
 
--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,5 @@
 /*
-Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
+Copyright © 2023 Ivan Shamrai isshamray@gmail.com
 */
 package cmd
 
@@ -10,21 +9,10 @@ import (
 	"github.com/spf13/cobra"
 )
 
-
-
-// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "aeroflot-api-cli",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Short: "Cli tool to interact with Aeroflot api",
+	Long:  `Interaction with Aeroflot API to get required information in reasonable time in compare with manual grabbing.`,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -45,7 +33,5 @@ func init() {
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
